2d629b18b7b91660ea2fd0b122894977ee96e72b
rupalavanya <rupalavanya780@gmail.com>
2026-09-16 17:50:22 +0530

    Add Spring Boot project setup

---
+++ src/test/java/com/flipkart_project/FlipkartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipkart_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
